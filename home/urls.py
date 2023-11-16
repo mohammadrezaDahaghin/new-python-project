d48d5f28cf3bf73a6cb1b21fce54582fcb1fd5e1
mamal <kitkat9069@gmail.com>
2023-11-16 15:09:37 +0330

    delete item in Detail

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),
-    path('detail/<int:todo_id>/',views.detail,name='details')
+    path('detail/<int:todo_id>/',views.detail,name='details'),
+    path('delete/<int:todo_id>/',views.delete,name='delete')
 ]
