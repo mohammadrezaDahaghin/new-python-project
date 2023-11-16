2485476273c5411b51cf044ecebc50dc86ba33ed
mamal <kitkat9069@gmail.com>
2023-11-16 22:07:47 +0330

    submit form

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class TodoCreateForm(forms.Form):
-    title=forms.CharField()
-    body=forms.CharField()
+    title=forms.CharField(label='عنوان',required=False)
+    body=forms.CharField(label='توضیحات')
     created=forms.DateTimeField()
--- home/views.py
+++ home/views.py
@@ -20,5 +20,13 @@ def delete(request,todo_id):
 
 
 def create(request):
-    form=TodoCreateForm()
+    if request.method =='POST':
+        form=TodoCreateForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            Todo.objects.create(title=cd['title'], body=cd['body'],created=cd['created'])
+            messages.success(request,'Todo creted Successfully','success')
+            return redirect('home')
+    else:
+        form=TodoCreateForm()
     return render(request,'create.html',{'form': form})
