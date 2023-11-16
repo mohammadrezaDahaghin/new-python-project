11a7f60fe9a60ddba3ff937ac6856425d38e0e1b
mamal <kitkat9069@gmail.com>
2023-11-16 13:15:38 +0330

    name URL

--- home/views.py
+++ home/views.py
@@ -5,9 +5,6 @@ from .models import Todo
 def home(request):
     allTodo=Todo.objects.all()
     return render(request, 'home.html',{'todos':allTodo})
-def say_hello(request):
-
-    return render(request, 'Hello.html', {'name': 'amin'})
 
 def detail(request,todo_id):
     todo=Todo.objects.get(id=todo_id)
