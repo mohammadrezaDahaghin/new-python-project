edf5315fc163a968c3475f28f8a38d0ff52d5237
mamal <kitkat9069@gmail.com>
2023-11-16 12:46:00 +0330

    link to page detil

--- home/views.py
+++ home/views.py
@@ -8,3 +8,7 @@ def home(request):
 def say_hello(request):
 
     return render(request, 'Hello.html', {'name': 'amin'})
+
+def detail(request,todo_id):
+    todo=Todo.objects.get(id=todo_id)
+    return render(request,'detail.html',{'todo':todo})
