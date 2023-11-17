75cac632224174a503b3985417fff96dd5eac216
mamal <kitkat9069@gmail.com>
2023-11-17 20:30:48 +0330

    login user

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns=[
     path('register/',views.user_register,name='user_reg'),
+    path('login/',views.user_login,name='user_login')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm,UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -22,3 +23,21 @@ def user_register(request):
         form = UserRegistrationForm()
     return render(request,'register.html',{'form':form})
 
+
+
+def user_login(request):
+    if request.method =='POST':
+        form=UserLoginForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            user=authenticate(request,username=cd['username'],password=cd['password'])
+            if user is not None:
+                login(request,user)
+                messages.success(request,'user login Successfuly','success')
+                return redirect('home')
+            else:
+                messages.error(request, 'user not exist', 'danger')
+
+    else:
+        form=UserLoginForm()
+    return render(request,'login.html',{'form':form})
